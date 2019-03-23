c5e01093e6160dea0b41429b6d12dbc3a8355c81
Dustin Firebaugh <dafirebaugh@gmail.com>
2019-03-23 14:05:05 -0400

    expanding more properties into neighbors

--- project.js
+++ project.js
@@ -7,8 +7,14 @@ const projectFactory = ({ cost, start, end, neighbors }) => {
     rate: cost === "high" ? 85 : 75,
     travelRate: cost === "high" ? 55 : 45,
     neighbors: {
-      prev: neighbors[0] && date_diff_indays(neighbors[0].end, start),
-      next: neighbors[1] && date_diff_indays(end, neighbors[1].start)
+      prev: neighbors[0] && {
+        offset: date_diff_indays(neighbors[0].end, start),
+        ...neighbors[0]
+      },
+      next: neighbors[1] && {
+        offset: date_diff_indays(end, neighbors[1].start),
+        ...neighbors[1]
+      }
     }
   };
 };
